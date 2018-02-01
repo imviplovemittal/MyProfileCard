92e06072ce065610235fcc574b58c9932fe29858
Viplove <viplovemittal2@gmail.com>
2018-02-01 19:22:32 +0530

    Profile background updated

--- app/src/main/java/com/monkwish/myprofilecard/MainActivity.kt
+++ app/src/main/java/com/monkwish/myprofilecard/MainActivity.kt
@@ -20,13 +20,13 @@ class MainActivity : AppCompatActivity() {
         profileDownButton = findViewById(R.id.profile_down_button)
         profileDownButton?.setOnClickListener {
             if(linear_layout.visibility==View.INVISIBLE) {
-                var slide_down: Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.slide_down)
-                linear_layout.startAnimation(slide_down)
+                //var slide_down: Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.slide_down)
+                //linear_layout.startAnimation(slide_down)
                 linear_layout.visibility = View.VISIBLE
                 profileDownButton?.setImageResource(android.R.drawable.arrow_up_float)
             }else{
-                var slide_up: Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.slide_up)
-                linear_layout.startAnimation(slide_up)
+                //var slide_up: Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.slide_up)
+                //linear_layout.startAnimation(slide_up)
                 linear_layout.visibility = View.INVISIBLE
                 profileDownButton?.setImageResource(android.R.drawable.arrow_down_float)
             }
